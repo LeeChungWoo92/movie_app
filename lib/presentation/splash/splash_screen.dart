87f20c64d8e26df251d7f68cde62244a72207963
lcw <skcjddn@gmail.com>
2024-09-24 22:50:49 +0900

    chore: 시작화면 변경

--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 2), () {
-      context.go('/movie_list');
+      context.go('/bottom_navigation');
     });
   }
   @override
