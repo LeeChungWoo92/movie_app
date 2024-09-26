b67c8cccdf1646f8b031dcd3e6ed176c069ffcb2
lcw <skcjddn@gmail.com>
2024-09-26 15:58:38 +0900

    feat: 영화리스트 화면,데이터 연동

--- lib/presentation/movie/movie_list_screen.dart
+++ lib/presentation/movie/movie_list_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/presentation/component/movie_card_widget.dart';
+import 'package:movie_app/presentation/movie/movie_list_view_model.dart';
+import 'package:provider/provider.dart';
 
 class MovieListScreen extends StatelessWidget {
-  const MovieListScreen({super.key});
+  const MovieListScreen({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: movieList(context),
+    );
+  }
+
+  Widget movieList(BuildContext context) {
+    final viewModel = context.watch<MovieListViewModel>();
+    final movies = viewModel.state.movie;
+    return GridView.builder(
+      itemCount: movies.length,
+      itemBuilder: (context, index) {
+        final movie = movies[index];
+        print('무비:$movie');
+        if (viewModel.state.isLoading) {
+          const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return MovieCardWidget(movie: movie);
+      },
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+    );
   }
 }
