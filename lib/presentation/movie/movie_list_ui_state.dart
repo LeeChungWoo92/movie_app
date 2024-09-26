40ee8a11be57e9c368417c094db4c7c6c803d391
lcw <skcjddn@gmail.com>
2024-09-26 15:56:32 +0900

    feat: ui state 추가

---
+++ lib/presentation/movie/movie_list_ui_state.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../domain/model/movie.dart';
+
+part 'movie_list_ui_state.freezed.dart';
+
+part 'movie_list_ui_state.g.dart';
+
+@freezed
+class MovieListUiState with _$MovieListUiState {
+  const factory MovieListUiState({
+    @Default([]) List<Movie> movie,
+    @Default(false) bool isLoading,
+    @Default('에러발생') String errorMessage,
+  }) = _MovieListUiState;
+
+  factory MovieListUiState.fromJson(Map<String, Object?> json) => _$MovieListUiStateFromJson(json);
+}
