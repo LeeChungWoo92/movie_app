2a3f22bb5cfdd70d67bed7ba5b8640ee6dca6201
lcw <skcjddn@gmail.com>
2024-09-26 15:59:40 +0900

    feat: di추가

---
+++ lib/di/di_setup.dart
@@ -0,0 +1,17 @@
+import 'package:get_it/get_it.dart';
+import 'package:movie_app/data/datasource/movie_data_source.dart';
+import 'package:movie_app/domain/repository/moive_repository.dart';
+import 'package:movie_app/presentation/movie/movie_list_view_model.dart';
+
+import '../data/datasource/movie_data_source_impl.dart';
+import '../data/repository/movie_repository_impl.dart';
+
+final getIt = GetIt.instance;
+
+void diSetUp() {
+  //싱글턴
+  getIt.registerSingleton<MovieDataSource>(MovieDataSourceImpl());
+  getIt.registerSingleton<MovieRepository>(MovieRepositoryImpl(getIt()));
+  //팩토리
+  getIt.registerFactory<MovieListViewModel>(() => MovieListViewModel(getIt()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'di/di_setup.dart';
 import 'router/router.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  diSetUp();
   runApp(const MyApp());
 }
 
