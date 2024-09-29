efa986038801ee1ec18482f6373b91b31964b93b
lcw <skcjddn@gmail.com>
2024-09-29 19:13:09 +0900

    rename: 오타 수정

--- lib/domain/repository/movie_repository.dart
+++ lib/domain/repository/movie_repository.dart
@@ -2,6 +2,6 @@ import '../../data/dto/movie_dto.dart';
 import '../model/movie.dart';
 
 abstract interface class MovieRepository {
-  Future<MovieDto> fetch();
-  Future<List<Movie>> getMovies();
+  Future<MovieDto> fetch(int page);
+  Future<List<Movie>> getMovies(int page);
 }
