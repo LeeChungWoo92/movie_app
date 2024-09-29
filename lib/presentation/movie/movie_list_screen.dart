c62ffe9a2768a7ad033060df334515b2257cc931
lcw <skcjddn@gmail.com>
2024-09-29 15:47:44 +0900

    feat: MoviDto추가

--- lib/presentation/movie/movie_list_screen.dart
+++ lib/presentation/movie/movie_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/presentation/component/movie_card_widget.dart';
+import 'package:movie_app/presentation/movie/movie_list_ui_state.dart';
 import 'package:movie_app/presentation/movie/movie_list_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -10,20 +11,48 @@ class MovieListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<MovieListViewModel>();
+    final state = viewModel.state;
+
     return Scaffold(
-      body: movieList(context),
-    );
+        backgroundColor: Colors.black,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 16,
+            ),
+            const Text(
+              '개봉일',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              '${state.movieData!.dates!.minimum.toString()} ~ ${state.movieData!.dates!.maximum.toString()}',
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Expanded(
+              child: movieList(state),
+            ),
+          ],
+        ));
   }
 
-  Widget movieList(BuildContext context) {
-    final viewModel = context.watch<MovieListViewModel>();
-    final movies = viewModel.state.movie;
+  Widget movieList(MovieListUiState state) {
+    final movies = state.movie;
+
     return GridView.builder(
+      shrinkWrap: true,
       itemCount: movies.length,
       itemBuilder: (context, index) {
         final movie = movies[index];
-        print('무비:$movie');
-        if (viewModel.state.isLoading) {
+        if (state.isLoading) {
           const Center(
             child: CircularProgressIndicator(),
           );
--- lib/presentation/movie/movie_list_ui_state.dart
+++ lib/presentation/movie/movie_list_ui_state.dart
@@ -1,5 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../../data/dto/movie_dto.dart';
 import '../../domain/model/movie.dart';
 
 part 'movie_list_ui_state.freezed.dart';
@@ -9,6 +10,7 @@ part 'movie_list_ui_state.g.dart';
 @freezed
 class MovieListUiState with _$MovieListUiState {
   const factory MovieListUiState({
+    MovieDto? movieData,
     @Default([]) List<Movie> movie,
     @Default(false) bool isLoading,
     @Default('에러발생') String errorMessage,
