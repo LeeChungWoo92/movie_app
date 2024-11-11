efa5e21edccaf673e21dbf9916309f7b34bcb678
lcw <skcjddn@gmail.com>
2024-11-12 02:18:34 +0900

    .

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -1,6 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:movie_app/data/datasource/movie_data_source.dart';
-import 'package:movie_app/domain/repository/moive_repository.dart';
+import 'package:movie_app/domain/repository/movie_repository.dart';
 import 'package:movie_app/presentation/movie/movie_list_view_model.dart';
 
 import '../data/datasource/movie_data_source_impl.dart';
